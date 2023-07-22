8a714bd8bccb42f6007fef1f62b8a95fb6f1f7ce
Zahji <hajjizahra25@hotmail.com>
2023-07-22 15:33:34 +0200

    001

--- YOUTUBE/Algorithmie.py
+++ YOUTUBE/Algorithmie.py
@@ -1,1 +1,13 @@
 # https://www.youtube.com/watch?v=iNeqb-URBj4&ab_channel=Foxxpy-Mathématiquesetalgorithmie
+
+# 001 Nombre premier ?
+    
+def is_prime(n):
+    for d in range(2,n):
+        if n % d == 0:
+            return False
+    return True
+
+
+
+
