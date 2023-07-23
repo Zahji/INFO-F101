483ddff6e5e6e11d817d8e6bbb955847ae471a1a
Zahji <hajjizahra25@hotmail.com>
2023-07-23 17:33:16 +0200

    016

--- YOUTUBE/Algorithmie.py
+++ YOUTUBE/Algorithmie.py
@@ -204,5 +204,10 @@ def anagrame(word1, word2):
     else:
         return False
 
+#016 Fibonacci ?
 
-
+def fibonacci(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
