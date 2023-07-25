ab14dff1402e05626ca72556973dedf4a78ed3e6
Zahji <hajjizahra25@hotmail.com>
2023-07-25 09:40:12 +0200

    044

--- YOUTUBE/Algorithmie.py
+++ YOUTUBE/Algorithmie.py
@@ -478,3 +478,13 @@ def inverse(string):
 def determinant_2(M):
     return M[0][0] * M[1][1] - M[0][1] * M[1][0]
 
+#044 Determinant Matrice 3 ?
+
+def determinant_3(M):
+    aei = M[0][0] * M[1][1] * M[2][2]
+    bfg = M[0][1] * M[1][2] * M[2][0]
+    cdh = M[0][2] * M[1][0] * M[2][1]
+    ceg = M[0][2] * M[1][1] * M[2][0]
+    bdi = M[0][1] * M[1][0] * M[2][2]
+    afh = M[0][0] * M[1][2] * M[2][1]
+    return aei + bfg + cdh - ceg - bdi - afh
