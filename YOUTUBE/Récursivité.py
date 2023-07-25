c30bce930f12c60ba404690e2f315eff92ee8b7d
Zahji <hajjizahra25@hotmail.com>
2023-07-25 14:02:44 +0200

    2

--- YOUTUBE/Récursivité.py
+++ YOUTUBE/Récursivité.py
@@ -1,7 +1,6 @@
 # https://www.youtube.com/watch?v=Dz-Izb1bUio&list=PL6JtJ0Q7T-YxUzIjSzUSXe18yWZ2jXtGz&index=1&ab_channel=Foxxpy-Mathématiquesetalgorithmie
 
-# Exercice 1
-
+#1
 def recursion_multiplication(n1, n2, i=1):
     if i < n2 :
         n1 = recursion_multiplication(n1, n2, i = i+1) + n1
@@ -12,3 +11,9 @@ n2 = 5
 resultat = recursion_multiplication(n1, n2)
 print("Récursion: " + str(resultat))
 print("Multiplication: " + str(n1*n2))
+
+#2
+def recursion_factorielle(n):
+    if n > 2:
+        n *= recursion_factorielle(n-1)
+    return n
