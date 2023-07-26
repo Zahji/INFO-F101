d8dfc9f06e01fdf0e654c0a63bc2a53c8d2624f1
Zahji <hajjizahra25@hotmail.com>
2023-07-26 14:46:58 +0200

    Q1.1

--- Examens/Tests/Partie Pratique.py
+++ Examens/Tests/Partie Pratique.py
@@ -2,4 +2,16 @@
 
 # ---------- 2012_2013 ---------- #
 
+# ---------- QUESTION 1 --------- #
 
+# 1) Programmation Impérative:
+
+# Déclaration de la variable pour stocker la somme
+somme = 0
+
+# Boucle pour ajouter les nombres de 1 à 5 à la somme
+for nombre in range(1, 6):
+    somme = somme + nombre
+
+# Affichage du résultat
+print("La somme est :", somme)
