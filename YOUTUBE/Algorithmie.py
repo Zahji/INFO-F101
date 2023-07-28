d31630c8f57a90ba4122c3a2ef9b20f4334b842e
Zahji <hajjizahra25@hotmail.com>
2023-07-28 19:05:10 +0200

    050

--- YOUTUBE/Algorithmie.py
+++ YOUTUBE/Algorithmie.py
@@ -527,3 +527,19 @@ def somme(liste):
         total += nombre
     return total
 
+#050
+
+#Remove : retire un élément spécifique de la liste
+liste = ["a", "b", "c", "d", "e", "f"]
+liste.remove("b")
+print(liste)
+
+#pop : retire un élément à un indice spécifique
+liste = ["a", "b", "c", "d", "e", "f"]
+liste.pop(2) #retire le c
+print(liste)
+
+#del : supprime un élément à un indice spécifique, mais ce n'est pas une méthode appartenant à la classe List.
+liste = ["a", "b", "c", "d", "e", "f"]
+del liste[3] #retire le d
+print(liste)
