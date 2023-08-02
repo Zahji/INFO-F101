cdd18eb4ddacdd4faff83c283df34e6f93ff1a33
Zahji <hajjizahra25@hotmail.com>
2023-08-02 18:43:47 +0200

    053

--- YOUTUBE/Algorithmie.py
+++ YOUTUBE/Algorithmie.py
@@ -543,3 +543,31 @@ print(liste)
 liste = ["a", "b", "c", "d", "e", "f"]
 del liste[3] #retire le d
 print(liste)
+
+# 051
+
+l=[]
+for i in range(2000, 3201):
+    if (i%7==0) and (i%5!=0):
+        l.append(str(i))
+
+print(','.join(l))
+
+#052
+
+def fact(x):
+    if x == 0:
+        return 1
+    return x * fact(x - 1)
+
+x=int(input())
+print(fact(x))
+
+#053
+
+n=int(input())
+d=dict()
+for i in range(1,n+1):
+    d[i]=i*i
+
+print(d)
