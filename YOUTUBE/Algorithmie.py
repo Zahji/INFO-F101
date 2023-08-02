8fb1aab9176bcb729c676992cdcbf45dea116eeb
Zahji <hajjizahra25@hotmail.com>
2023-08-02 18:44:45 +0200

    055

--- YOUTUBE/Algorithmie.py
+++ YOUTUBE/Algorithmie.py
@@ -583,3 +583,15 @@ class InputOutString(object):
     
     def printString(self):
         print(self.s.upper())
+
+#055
+
+import math
+c=50
+h=30
+value = []
+items=[x for x in input().split(',')]
+for d in items:
+    value.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
+
+print(','.join(value))
