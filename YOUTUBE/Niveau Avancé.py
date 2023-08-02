a495da26719ac39fa6e05a1f2290be8a463b7660
Zahji <hajjizahra25@hotmail.com>
2023-08-02 15:31:40 +0200

    2

--- YOUTUBE/Niveau Avancé.py
+++ YOUTUBE/Niveau Avancé.py
@@ -2,7 +2,7 @@
 
 # Premier Lien : 
 
-# Exos 1 :
+# Exo 1 :
 
 def nbCheminsDist(dist):
     if dist == 0:
@@ -12,3 +12,14 @@ def nbCheminsDist(dist):
         return 0
  
     return nbCheminsDist(dist-1)+nbCheminsDist(dist-2)+nbCheminsDist(dist-3)
+
+# Exo 2 :
+
+class Rectangle:
+    def __init__(self, longueur, largeur):
+        self.longueur = longueur
+        self.largeur = largeur
+    
+    def aire(self):
+        return self.longueur * self.largeur
+    
