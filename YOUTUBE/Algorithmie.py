a9427bbc69ab68f72a9262c408abfb25ba134ef6
Zahji <hajjizahra25@hotmail.com>
2023-08-03 20:30:05 +0200

    064

--- YOUTUBE/Algorithmie.py
+++ YOUTUBE/Algorithmie.py
@@ -692,10 +692,19 @@ class Circle(object):
     def area(self):
         return self.radius**2*3.14
 
-aCircle = Circle(2)
-print aCircle.area()
 
 #064
+
+class Rectangle(object):
+    def __init__(self, l, w):
+        self.length = l
+        self.width  = w
+
+    def area(self):
+        return self.length*self.width
+
+aRectangle = Rectangle(2,10)
+print(aRectangle.area())
 
 #065
 
