285e1ba70df2a733ff32dcda652bd47f229f3243
Zahji <hajjizahra25@hotmail.com>
2023-08-05 16:33:48 +0200

    1

--- Autres/F103.py
+++ Autres/F103.py
@@ -5,4 +5,13 @@ class Complexe:
         self.__re = re
         self.__im = im
 
-    
+    def re(self):
+        return self.__re
+    @property
+    def im(self):
+        return self.__im
+    def __str__(self):
+        return f"{self.re:g} + {self.im:g}i"
+    def __iadd__(self, z):
+        if isinstance(z, (int, float)): 
+            z = Complexe(re=z, im=0)
