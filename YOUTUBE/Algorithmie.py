410163cb0a8fc110b599204fa8f623d40fc16490
Zahji <hajjizahra25@hotmail.com>
2023-08-05 17:23:39 +0200

    105

--- YOUTUBE/Algorithmie.py
+++ YOUTUBE/Algorithmie.py
@@ -1402,6 +1402,13 @@ def table_multiplication(nombre, multiplicateur):
 
 #105
 
+def tri_bulles(liste):
+    n = len(liste)
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if liste[j] > liste[j + 1]:
+                liste[j], liste[j + 1] = liste[j + 1], liste[j]
+
 
 #106
 
